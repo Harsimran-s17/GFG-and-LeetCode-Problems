ba6019ce9643b515fbd48324f39b5263905b6fea
Harsimran Singh <108118129+Harsimran-s17@users.noreply.github.com>
2023-07-02 10:26:53 +0530

    Added solution - LeetHub

---
+++ Maximum Difference - GFG/maximum-difference.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    /*You are required to complete this method */
+    int findMaxDiff(int A[], int n)
+    {
+      //Your code here
+      stack<int> left;
+      stack<int> temp;
+      vector<int> vec;
+      
+      for(int i = 0;  i < n; i++){
+          if(temp.empty()) left.push(0);
+          else{
+              while(!temp.empty() && temp.top()>=A[i]){
+                  vec.push_back(temp.top());
+                  temp.pop();
+              }
+              if(temp.empty()) left.push(0);
+              else{
+                  left.push(temp.top());
+                  }
+                  while(!vec.empty()){
+                      temp.push(vec.back());
+                      vec.pop_back();
+              }
+          }
+        temp.push(A[i]);
+      }
+      while(!temp.empty()) temp.pop();
+      vec.clear();
+      stack<int> right;
+      
+      for(int i = n-1; i>=0; i--){
+          if(temp.empty()) right.push(0);
+          else{
+              while(!temp.empty() && temp.top()>=A[i]){
+                  vec.push_back(temp.top());
+                  temp.pop();
+              }
+              if(temp.empty()) right.push(0);
+              else{
+                  right.push(temp.top());
+                  }
+                  while(!vec.empty()){
+                      temp.push(vec.back());
+                      vec.pop_back();
+              }
+          }
+        temp.push(A[i]);
+      }
+      while(!temp.empty()) temp.pop();
+      
+      while(!right.empty()){
+          temp.push(right.top());
+          right.pop();
+      }
+      
+      int ans = 0;
+      
+      for(int i = 0; i < n; i++){
+        //   cout<<temp.top()<<" ";
+          ans = max(ans, abs(left.top() - temp.top()));
+        //   cout<<left.top()<<" "<<temp.top()<<endl;
+          left.pop();
+          temp.pop();
+      }
+      
+      
+      
+      return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+   	int n;
+   	cin>>n;
+   	int a[n];
+   	for(int i=0;i<n;i++)
+   	cin>>a[i];
+   	Solution ob;
+   	cout<<ob.findMaxDiff(a,n)<<endl;
+   }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
