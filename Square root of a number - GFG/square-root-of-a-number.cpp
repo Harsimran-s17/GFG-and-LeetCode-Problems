cb00a20e67314f2f1706bd78ec8e92d070949d6f
Harsimran Singh <108118129+Harsimran-s17@users.noreply.github.com>
2023-07-05 15:22:38 +0530

    Added solution - LeetHub

---
+++ Square root of a number - GFG/square-root-of-a-number.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        // Your code goes here  
+        // long long int l = 0; 
+        // long long int  h = x;
+        // long long int mid, ans;
+        
+        // while(l <=h){
+        //     mid = (l+h)/2;
+        //     if(mid*mid==x) return mid;
+        //     if(mid*mid < x) {
+        //         l = mid+1;
+        //         ans = mid;
+        //     }else{
+        //         h = mid - 1;
+        //     }
+        // }
+        // return ans;
+        
+        
+        //OR
+        
+        long long  low = 0;
+        long long  high = x;
+        long long ans = 1;
+        
+        while(low<=high){
+            long long mid = (low+high)>>1;
+            
+            if(mid*mid==x) return mid;
+            else if(mid*mid < x) {
+                ans = max(ans, mid);
+                low = mid+1;
+            }
+            else high = mid -1;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		Solution obj;
+		cout << obj.floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+
+// } Driver Code Ends
