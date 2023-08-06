9324151edab87fe3facedbeca5241b67c1e5ea70
Harsimran Singh <108118129+Harsimran-s17@users.noreply.github.com>
2023-08-06 09:46:40 +0530

    Time: 1362 ms (45.91%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -19,12 +19,21 @@
 
 # USING CASE STATEMENT
 
-select employee_id, 
-case
-    when employee_id%2=1 and name not like "M%" THEN salary 
-    else 0
-end
-as bonus
+# select employee_id, 
+# case
+#     when employee_id%2=1 and name not like "M%" THEN salary 
+#     else 0
+# end
+# as bonus
+# from employees
+# order by employee_id;
+
+
+# OR
+
+# SIMPLE
+
+select employee_id, salary * (employee_id%2) * (name not like "M%") as bonus 
 from employees
 order by employee_id;
 
