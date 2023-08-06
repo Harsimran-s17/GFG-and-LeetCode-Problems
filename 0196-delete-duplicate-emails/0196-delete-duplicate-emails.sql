164cd4eb4fbcc8f7613afe5ac95a323de65d9d03
Harsimran Singh <108118129+Harsimran-s17@users.noreply.github.com>
2023-08-06 10:37:41 +0530

    Time: 1136 ms (96.64%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
 
-delete p from person as p, person as s where p.id>s.id and p.email=s.email;
+# delete p from person as p, person as s where p.id>s.id and p.email=s.email;
+
+# OR
+with cte as (select * from person)
+delete from person where id not in(select min(id) from cte group by email);
