64b4b19c4d60eecbe0f87611eb787831e8e7ba74
Harsimran Singh <108118129+Harsimran-s17@users.noreply.github.com>
2023-08-06 10:09:31 +0530

    Time: 454 ms (85.21%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,6 +1,20 @@
 # Write your MySQL query statement below
-update salary set sex = 
-case 
-    when sex="m" then "f"
-    else "m"
-end;
+
+# USING CASE STATEMENT
+
+# update salary set sex = 
+# case 
+#     when sex="m" then "f"
+#     else "m"
+# end;
+
+# OR
+
+# replace() function
+
+update salary set sex= replace('fm',sex,'');
+
+
+
+
+
