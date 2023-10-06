cce69b684ddd554e24da3da06b102795bf6e8ff8
Harsimran Singh <108118129+Harsimran-s17@users.noreply.github.com>
2023-10-06 12:46:26 +0530

    Added solution - LeetHub

---
+++ Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
@@ -0,0 +1,214 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(){
+        data = 0;
+        left = right = NULL;
+    }
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+// } Driver Code Ends
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution
+{
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
+    
+    bool solve(Node *root, int n1,  vector<Node *> &mp){
+        if(!root) {
+            return false;
+        }
+        
+        if(root->data == n1) {
+            mp.push_back(root);
+            return true;
+        }
+        bool f1 = solve(root->left, n1,mp);
+        bool f2 = solve(root->right, n1,mp);
+        if (f1||f2) mp.push_back(root);
+        return (f1|f2);
+    }
+    
+    Node* ans(Node * root, int n, unordered_map<Node*, int> m, Node* &ansr, bool &flag){
+        if(root->data == n) {
+            if(m.find(root)!=m.end() && flag) {
+            ansr = root;
+            flag = false;
+            return NULL;
+        }
+            return root;
+        }
+        Node * t1 = NULL;
+        Node * t2 = NULL;
+        if(root->left) t1 = ans(root->left, n, m, ansr, flag);
+        if(root->right) t2 = ans(root->right, n, m,ansr, flag);
+        if((t1||t2) && m.find(root)!=m.end() && flag) {
+            ansr = root;
+            flag = false;
+            return NULL;
+        }
+        if(t1) return t1;
+        else if(t2) return t2;
+        else return NULL;
+    }
+    
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       //Your code here 
+    //   unordered_map<Node*,int> m;
+    //   unordered_map<Node*,int> mp;
+    vector<Node *> vec1;
+    vector<Node *> vec2;
+    
+       Node * ansr = NULL;
+       solve(root, n1,vec1);
+       solve(root, n2, vec2);
+    //   if(root) vec1.push_back(root);
+    //   if(root) vec2.push_back(root);
+       reverse(vec1.begin(), vec1.end());
+       reverse(vec2.begin(), vec2.end());
+       int n = min(vec1.size(),vec2.size());
+       for(int i = 0 ; i< n;i++){
+          if(vec1[i]!= vec2[i]) break;
+           ansr = vec1[i];
+       }
+    //   bool flag = true;
+    //   ans(root,n2,mp, ansr, flag);
+       return ansr;
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // for(string i:ip)
+    //     cout<<i<<" ";
+    // cout<<endl;
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d %d ",&a,&b);
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout<<ob.lca(root,a,b)->data<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
