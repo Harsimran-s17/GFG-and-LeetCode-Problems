18d12971874f1339e94e78b925aaccc45a7587e6
Harsimran Singh <108118129+Harsimran-s17@users.noreply.github.com>
2023-10-14 08:35:28 +0530

    Added solution - LeetHub

---
+++ Tree from Postorder and Inorder - GFG/tree-from-postorder-and-inorder.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+Node* buildTree(int in[], int post[], int n);
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Node* root = buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+//Function to return a tree created from postorder and inoreder traversals.
+
+Node * solve(int in[], int post[], int i, int n, int s){
+    if(i>=n) return NULL;
+    Node * root = new Node(post[s]);
+    int r = post[s];
+    int j = n-1;
+    int l = s-1;
+    while(in[j]!=r){
+        j--;
+        s--;
+    }
+    root->left = solve(in,post,i,j,s-1);
+    root->right = solve(in,post,j+1,n,l);
+    
+}
+
+Node *buildTree(int in[], int post[], int n) {
+    // Your code here
+    return solve(in,post,0,n,n-1);
+}
